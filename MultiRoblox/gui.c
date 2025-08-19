965a035a104d3dfe0c7c94be8e6c005191d665dc
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2025-08-19 20:23:29 +0100

    smaller ui
    was big and got everywhere :trollface:

--- MultiRoblox/gui.c
+++ MultiRoblox/gui.c
@@ -1,6 +1,8 @@
 #include <windows.h>
 
 #include "gui.h"
+#include "mutex.h"
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -24,9 +26,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     RegisterClass(&wc);
 
-    g_hwnd = CreateWindowEx(0, "MULTIROBLOX_CLASS", L"MultiRoblox", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 400, 455, NULL, NULL, hInstance, NULL);
-    g_title = CreateWindow(L"STATIC", L"MultiRoblox 0.0.3", WS_VISIBLE | WS_CHILD, 10, 10, 365, 20, g_hwnd, NULL, hInstance, NULL);
-    g_textbox = CreateWindow(L"EDIT", L"Thanks for using MultiRoblox!\r\n\r\nIn case if you're wondering, you must open\r\nMultiRoblox FIRST before opening multiple ROBLOX\r\nsessions. Once you close MultiRoblox, all but one\r\nROBLOX instance will close.\r\n\r\nDiscord: https://discord.gg/g2Jgw8Z6Rb\r\nWeAreDevs: https://forum.wearedevs.net/t/29695\r\nGitHub: https://github.com/Dashbloxx/MultiRoblox\r\n", WS_BORDER | WS_VISIBLE | WS_CHILD | ES_LEFT | ES_AUTOHSCROLL | ES_MULTILINE | ES_READONLY, 10, 40, 365, 365, g_hwnd, NULL, hInstance, NULL);
+    g_hwnd = CreateWindowEx(0, "MULTIROBLOX_CLASS", L"MultiRoblox", WS_BORDER | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, CW_USEDEFAULT, CW_USEDEFAULT, 200, 120, NULL, NULL, hInstance, NULL);
+    g_title = CreateWindow(L"STATIC", L"MultiRoblox 0.0.4", WS_VISIBLE | WS_CHILD, 10, 10, 365, 20, g_hwnd, NULL, hInstance, NULL);
+    g_textbox = CreateWindow(L"EDIT", L"Please Open Roblox.This\r\nApplies Automatically", WS_BORDER | WS_VISIBLE | WS_CHILD | ES_LEFT | ES_AUTOHSCROLL | ES_MULTILINE | ES_READONLY, 10, 40, 365, 365, g_hwnd, NULL, hInstance, NULL);
     if (g_hwnd == NULL)
     {
         return 0;
